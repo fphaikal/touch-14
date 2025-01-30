271e2f1e08e43d77802fe0600de447d7f18efd1c
Fahreza Pasha Haikal <abufahreza@gmail.com>
2025-01-30 19:12:36 +0700

    Add motion animations to Register component and update WhatsApp links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Link } from "@heroui/link";
 import { button as buttonStyles } from "@heroui/theme";
+import * as motion from "motion/react-client"
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import Image from "next/image";
 import Countdown from "@/components/countdown";
-
 import { Register } from "@/components/register";
+
 
 export default function Home() {
   return (
@@ -20,15 +21,33 @@ export default function Home() {
           </div>
           <div className="flex items-center justify-between gap-4 w-full my-20 px-6">
             <div className="flex flex-col w-full sm:w-1/2 z-10 justify-center items-center sm:items-start text-center sm:text-start">
-              <div className="justify-center">
+              <motion.div
+                initial={{ opacity: 0.0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{
+                  type: "spring", visualDuration: 0.4, bounce: 0.25
+                }}
+                className="justify-center">
                 <p className={title()}>Tryout&nbsp;</p><p className={title({ color: "green" })}>Chemicalistronic</p>
                 <br />
                 <span className={subtitle()}>Paving Futures One Drop At A Time</span>
-                <div className="flex flex-col py-5">
-                  <Countdown targetDate="2025-02-16T07:15:00" />
-                </div>
-              </div>
-              <div className="flex gap-3 mt-4">
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0.0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{
+                  type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.1
+                }}
+                className="flex flex-col py-5">
+                <Countdown targetDate="2025-02-16T07:15:00" />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0.0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{
+                  type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.2
+                }}
+                className="flex gap-3 mt-4">
                 <Link
                   isExternal
                   className={buttonStyles({
@@ -40,52 +59,100 @@ export default function Home() {
                 >
                   Join Now
                 </Link>
-              </div>
+              </motion.div>
             </div>
-            <div className="hidden sm:flex justify-center items-center w-1/2 z-10">
+            <motion.div
+              initial={{ opacity: 0.0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.3
+              }}
+              className="hidden sm:flex justify-center items-center w-1/2 z-10">
               <Image src="/asset3.png" alt="Asset 1" width={500} height={500} />
-            </div>
+            </motion.div>
           </div>
-          <div className="flex flex-col items-center justify-center gap-4">
+          <motion.div
+            initial={{ opacity: 0.0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.4
+            }}
+            className="flex flex-col items-center justify-center gap-4">
             <span className="text-medium">Supported By:</span>
             <Image src="/logo-cendekia.png" alt="Cendekia" width={200} height={50} />
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="relative flex flex-col items-center justify-center gap-16 px-6 text-center">
         <div className="absolute top-0 -translate-y-[20rem] md:-translate-y-[40rem] lg:-translate-y-[40rem] xl:-translate-y-[60rem] -z-10 w-4/5 rounded-full aspect-square bg-green-600/70 backdrop-filter blur-3xl opacity-50" />
-        <span className={title({ size: "sm" })}>Succeed in ASPD with <span className={title({ size: "sm", color: "green" })}>TOUCH #14!</span></span>
+        <motion.span
+          initial={{ opacity: 0.0, y: -100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring", bounce: 0.25
+          }}
+          className={title({ size: "sm" })}>Succeed in ASPD with <span className={title({ size: "sm", color: "green" })}>TOUCH #14!</span></motion.span>
+
         <div className="grid lg:grid-cols-3 gap-4">
-          <div className="flex flex-col items-center justify-center text-center">
+          <motion.div
+            initial={{ opacity: 0.0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.1
+            }}
+            className="flex flex-col items-center justify-center text-center">
             <div className="bg-yellow-100 shadow-md shadow-yellow-100 rounded-3xl p-4 mb-6">
               <Image src="/pencil.svg" alt="Icon 1" width={30} height={30} className="drop-shadow " />
             </div>
             <span className={title({ size: "tiny" })}>ASPD Try Out Simulation</span>
             <p className={subtitle()}>Helps 9th-grade students prepare for ASPD with standardized test questions.</p>
-          </div>
-          <div className="flex flex-col items-center justify-center text-center lg:border-l lg:border-r border-divider ">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0.0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.2
+            }}
+            className="flex flex-col items-center justify-center text-center lg:border-l lg:border-r border-divider ">
             <div className="bg-blue-100 shadow-md shadow-blue-100 rounded-3xl p-4 mb-6">
               <Image src="/book.svg" alt="Icon 1" width={30} height={30} className="drop-shadow" />
             </div>
             <span className={title({ size: "tiny" })}>Focused Academic Preparation</span>
             <p className={subtitle()}>Enhances students' academic skills before taking the ASPD exam.</p>
-          </div>
-          <div className="flex flex-col items-center justify-center text-center">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0.0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.3
+            }}
+            className="flex flex-col items-center justify-center text-center">
             <div className="bg-green-100 shadow-md shadow-green-100 rounded-3xl p-4 mb-6">
               <Image src="/school.svg" alt="Icon 1" width={30} height={30} className="drop-shadow " />
             </div>
             <span className={title({ size: "tiny" })}>Introduction to SMK SMTI Yogyakarta</span>
             <p className={subtitle()}>Provides participants with an opportunity to explore SMK SMTI Yogyakarta.</p>
-          </div>
+          </motion.div>
         </div>
       </div>
-      <div className="relative flex flex-col min-h-screen items-center justify-center px-6 py-44 lg:py-64 text-center">
+      <div className="relative flex flex-col min-h-screen items-center justify-center px-6 py-44 lg:py-80 text-center">
         <div className="absolute top-0 left-0 -translate-x-[60rem] w-4/5 -z-10 rounded-full aspect-square bg-gradient-to-t from-green-600/70 backdrop-filter blur-3xl opacity-35" />
-        <span className={title({ size: "sm" })}><span className={title({ color: "green", size: "sm" })}>TOUCH #14</span> Try Out Chemicalistronic</span>
-        <span className={subtitle({ className: "text-center" })}>16 Februari 2025 – SMK SMTI Yogyakarta</span>
+        <motion.span
+          initial={{ opacity: 0.0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring", visualDuration: 0.4, bounce: 0.25
+          }}
+          className={title({ size: "sm" })}><span className={title({ color: "green", size: "sm" })}>TOUCH #14</span> Try Out Chemicalistronic</motion.span>
+        <motion.span
+          initial={{ opacity: 0.0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.1
+          }}
+          className={subtitle({ className: "text-center" })}>16 Februari 2025 – SMK SMTI Yogyakarta</motion.span>
         <div className="flex w-full mt-10 justify-center">
           <Register />
-
         </div>
       </div>
     </section>
--- components/register.tsx
+++ components/register.tsx
@@ -4,6 +4,8 @@ import { Card, CardBody, CardFooter, CardHeader, Link } from "@heroui/react";
 import { button as buttonStyles } from "@heroui/theme";
 import { subtitle, title } from "./primitives";
 import { IconBrandWhatsapp } from "@tabler/icons-react";
+import * as motion from "motion/react-client"
+
 
 const items = [
   {
@@ -47,33 +49,51 @@ export function Register() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full place-items-center">
-        {items.map((item) => (
-          <Card isHoverable key={item.participant} className="w-full h-full">
-            <CardHeader className="text-center">
-              <span className={subtitle({ className: "text-center" })}>{item.participant} Participants</span>
-            </CardHeader>
-            <CardBody className="py-5 px-16">
-              <span className={title({ className: "text-center", color: "green", size: "lg" })}>{item.price}</span>
-              <br />
-              <span className={title({ className: "text-center", size: "tiny" })}>{item.title}</span>
-              <span className='text-center text-default-400'>{item.description}</span>
-            </CardBody>
-            <CardFooter className="text-center justify-center">
-              <Link
-                isExternal
-                className={buttonStyles({
-                  color: "success",
-                  radius: "full",
-                  variant: "shadow",
-                })}
-                href={item.link}
-              >
-                Register
-              </Link>
-            </CardFooter>
-          </Card>
+        {items.map((item, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0.0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring", visualDuration: 0.4, bounce: 0.25, delay: index * 0.1
+            }}
+          >
+            <Card isHoverable key={item.participant} className="w-full h-full drop-shadow">
+              <CardHeader className="text-center">
+                <span className={subtitle({ className: "text-center" })}>{item.participant} Participants</span>
+              </CardHeader>
+              <CardBody className="py-5 px-16">
+                <span className={title({ className: "text-center", color: "green", size: "lg" })}>{item.price}</span>
+                <br />
+                <span className={title({ className: "text-center", size: "tiny" })}>{item.title}</span>
+                <span className='text-center text-default-400'>{item.description}</span>
+              </CardBody>
+              <CardFooter className="text-center justify-center">
+                <Link
+                  isExternal
+                  className={buttonStyles({
+                    color: "success",
+                    radius: "full",
+                    variant: "shadow",
+                  })}
+                  href={item.link}
+                >
+                  Register
+                </Link>
+              </CardFooter>
+            </Card>
+          </motion.div>
         ))}
-        <div className="flex flex-col justify-center items-center w-full h-72 lg:h-full">
+        <motion.div
+          initial={{ opacity: 0.0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring", visualDuration: 0.4, bounce: 0.25, delay: 0.6
+          }}
+          className="relative flex flex-col justify-center items-center rounded-medium w-full h-72 lg:h-full overflow-hidden drop-shadow border border-divider">
+          <div className="absolute top-0 left-0 -translate-x-10 -translate-y-28 -z-10 w-3/5 rounded-full aspect-square bg-green-600/70 backdrop-filter blur-3xl opacity-50" />
+          <div className="absolute bottom-0 right-0 translate-x-28 translate-y-24 -z-10 w-3/5 rounded-full aspect-square bg-green-600/70 backdrop-filter blur-3xl opacity-50" />
+
           <span className={title({ size: "tiny" })}>Contact Us for More Info</span>
           <span className='text-center text-default-400'>Got questions? Reach out to us:</span>
           <Link
@@ -83,7 +103,7 @@ export function Register() {
               radius: "full",
               variant: "shadow",
             })} flex justify-center items-center mt-4`}
-            href="wa.me/6281542780426"
+            href="https://wa.me/6281542780426"
           >
             <IconBrandWhatsapp />
             Contact Arif
@@ -95,12 +115,12 @@ export function Register() {
               radius: "full",
               variant: "shadow",
             })} flex justify-center items-center mt-4`}
-            href="wa.me/6287826540014"
+            href="https://wa.me/6287826540014"
           >
             <IconBrandWhatsapp />
             Contact Leny
           </Link>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
